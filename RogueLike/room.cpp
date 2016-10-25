9175ef0535954ad4859acd89852ef06390cb07a4
Patrick Leijser <patrick@breaky.eu>
2016-10-25 21:23:23 +0200

    Room domain class

--- RogueLike/room.cpp
+++ RogueLike/room.cpp
@@ -1,14 +1,33 @@
 #include "stdafx.h"
 #include "room.h"
 
-Room::Room(int s, bool d, bool f)
+Room::Room()
 {
-	size = s;
-	dirty = d;
-	furniture = f;
 }
 
 std::string Room::GetDescription()
 {
 	return std::string();
+}
+
+void Room::GenerateRoom() {
+	std::string sizes[3] { "big room", "normal room", "small room" };
+	std::string dirtyness[2]{ "clean", "dirty" };
+	std::string furnitures[3]{ "with a table with four chairs", "with a bed", "with nothing" };
+
+	SetSize(sizes[1]);
+	SetDirty(dirtyness[1]);
+	SetFurniture(furnitures[1]);
+}
+
+void Room::SetSize(const std::string & s) {
+	size_ = s;
+}
+
+void Room::SetDirty(const std::string & d) {
+	dirty_ = d;
+}
+
+void Room::SetFurniture(const std::string & f) {
+	furniture_ = f;
 }
--- RogueLike/room.h
+++ RogueLike/room.h
@@ -2,11 +2,24 @@
 
 class Room
 {
-	int size;
-	bool dirty;
-	bool furniture;
-
 public:
-	Room(int size,bool dirty,bool furniture);
+	Room();
+	~Room();
+
+	// Getters
 	std::string GetDescription();
+
+	// Setters
+	void SetSize(const std::string & s);
+	void SetDirty(const std::string & d);
+	void SetFurniture(const std::string & f);
+
+private:
+	std::string size_;
+	std::string dirty_;
+	std::string furniture_;
+	std::string type_;
+
+	// Methods
+	void GenerateRoom();
 };
